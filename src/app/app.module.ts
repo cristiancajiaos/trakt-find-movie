000c42ad8987ebe71d577fd17b2e22f1c4f8891a
Cristian Cajiao <cristiancajiaos@outlook.com>
2022-04-19 14:55:02 -0400

    Creado interceptor de manejo de errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './layout/layout.component';
+import { ErrorHandleInterceptor } from './shared/interceptors/error-handle.interceptor';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,11 @@ import { LayoutComponent } from './layout/layout.component';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: ErrorHandleInterceptor,
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/interceptors/error-handle.interceptor.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+
+@Injectable()
+export class ErrorHandleInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        let errorMsg = '';
+        if (error.error instanceof ErrorEvent) {
+          errorMsg = `Error Message: ${error.message}`;
+        } else {
+          errorMsg = `Error Code: ${error.status}, Error Message: ${error.message}`;
+        }
+
+        return throwError(() => new Error(errorMsg));
+      })
+    )
+  }
+}
